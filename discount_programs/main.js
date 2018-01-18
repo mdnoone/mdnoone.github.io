097514975198230f93003ba2d5070e171cc8bbe2
mdnoone <matthew.d.noone@gmail.com>
2018-01-18 16:03:20 -0600

    discount_programs

--- discount_programs/main.js
+++ discount_programs/main.js
@@ -1,14 +1,22 @@
 
 var geoC = (function() {
 	// declare variables
-	var geocoderControl = false;	// controls the double search error
-	var titleText = "Wisconsin Discount Broadband Programs";	// text for title
-	var subText = "Enter your address and search for what programs are available.";	// optional text for sub title
-	var errorText = "It appears something has gone wrong. Please search this address again, or choose a different address.";	// text for error message
-	var errorText2 = "The address you have chosen is not valid. Please choose a new address.";	// text for error message
-	var errorCount = 0;		// controls which error message is displayed
+	var geocoderControl = false;
+	var titleText = "Internet Discount Finder";
+	var subText = "Enter your address to search for available programs.";	// optional text for sub title
 	
-	// create and connect to map (using bordner lab mapbox account)
+	var subText = "<center>Contact the State Broadband Office</center>";
+	document.write("<p>" + subText.link("https://psc.wi.gov/Pages/Programs/BroadbandTeam.aspx") + "</p>");
+	var subText = "<p>Internet providers offer an array of discounted internet subscriptions based upon a variety of factors such as income, participation in Free and Reduced Cost Lunch programs, housing assistance, veteran status, Medicaid, Supplementary Security Income and more. The Wisconsin State Broadband Office has developed a tool to help identify programs available based upon location.<br>" +
+	"<br><b> Enter your address and search for what programs are available. Data is current as of Decemeber 2016 </br></p>";	// optional text for sub title	
+	
+	//var loaderText = "Loading...";
+	//var subText = "";
+	var errorText = "It appears something has gone wrong. Please search this address again, or choose a different address.";
+	var errorText2 = "The address you have chosen is not valid. Please choose a new address.";
+	var errorCount = 0;
+	
+	// create and connect to map
 	mapboxgl.accessToken = 'pk.eyJ1IjoiYm9yZG5lcndsZWkiLCJhIjoiY2lyZjd1a2tyMDA3dmc2bmtkcjUzaG5meCJ9.eswxCZSAnob59HR0wEaTpA';
 	var map = new mapboxgl.Map({
 		container: 'map',
@@ -32,23 +40,13 @@ var geoC = (function() {
 	var geocoder = new MapboxGeocoder({
 		accessToken: mapboxgl.accessToken,
 		placeholder: "search...",
-	//	country: 'us',
+		country: 'us',
 		limit: 6,
-	//	proximity: [44.3022,-89.6852],
-	//	bbox: [-93.688669,42.362197,-86.755047,47.014527],
-	//	autocomplete: false,
-	//	postcode: '53714'
-		//region: 'new york'
+		bbox: [-93.688669,42.362197,-86.755047,47.014527],
 	});
 	
 	// add geocoder 
 	map.addControl(geocoder);
-	
-	//map.addControl(new MapboxGeocoder({
-	//	accessToken: mapboxgl.accessToken,
-	//	country: 'us'
-	//}));
-	
 	
 	// remove and append geocoder
 	$('.mapboxgl-ctrl-geocoder').detach().appendTo('#myContainer');
@@ -60,7 +58,7 @@ var geoC = (function() {
 	var myLine = document.createElement("hr");
 	$(myLine).appendTo("#myContainer");
 	
-	// create and assign loader bar for geocoder
+	
 	var myLoaderBox = document.createElement("div");
 	var myLoader = document.createElement("div");
 	myLoaderBox.setAttribute("id", "myProgress");
@@ -68,16 +66,16 @@ var geoC = (function() {
 	$(myLoader).appendTo(myLoaderBox);
 	$(myLoaderBox).appendTo("#myContainer");
 	
+	
 	// ensures map has loaded before continuing
 	map.on('load', function() {
-		
-		// add source for programs polygon layer
+	
 		map.addSource('programs', {
 			'type': 'geojson',
 			'data': 'programs.geojson'
 		});
 		
-		// add programs polygon layer
+		// add polygon layer
 		map.addLayer({
 			'id': 'program-poly',
 			'type': 'fill',
@@ -89,7 +87,7 @@ var geoC = (function() {
 			}
 		});
 		
-		// add point source (for geocoder)
+		// add point source
 		map.addSource('single-point', {
 			"type": "geojson",
 			"data": {
@@ -98,7 +96,7 @@ var geoC = (function() {
 			}
 		});
 		
-		// add point layer (for geocoder)
+		// add point layer
 		map.addLayer({
 			"id": "point",
 			"source": "single-point",
@@ -109,7 +107,6 @@ var geoC = (function() {
 			}
 		});
 		
-		// interval check to make sure that the program polygon layer is loaded before allowed to do anything else with geocoder
 		initLoad = setInterval(function(){
 			var checky = map.isSourceLoaded('programs');
 			if (checky == true) {
@@ -123,25 +120,23 @@ var geoC = (function() {
 
 		// Listen for the `geocoder.input` event
 		geocoder.on('result', function(ev) {
-			// on geocoder result, run moveBar function
 			moveBar(ev, ev.result.geometry.coordinates);
 		});
 	});
 	
 	function moveBar(ev, point) {
+				
 		// retrieve and remove all classes with 'gonnaRemove'
 		var para = document.getElementsByClassName('gonnaRemove');
 		while (para[0]) {
 			para[0].parentNode.removeChild(para[0]);
 		}
-		
-		// show loader
+					
 		myLoaderBox.style.visibility = "visible";
 		var elem = document.getElementById("myBar");
 		var width = 0;
 		var id = setInterval(frame, 50);
 		function frame() {
-			// if the loader bar is at 100%, clear loader, run query, and call function to add data to DOM
 			if (width >= 100) {
 				clearInterval(id);
 				myLoaderBox.style.visibility = "hidden";
@@ -151,33 +146,43 @@ var geoC = (function() {
 				var features = map.queryRenderedFeatures(point, { layers: ['program-poly'] });
 				var layer = features[0];
 				
-				// check if successful or unsuccessful
+				var poly = turf.polygon([[
+  					[-81, 41],
+  					[-81, 47],
+  					[-72, 47],
+  					[-72, 41],
+  					[-81, 41]
+				]]);
+
+				var isInside = turf.inside(ev.result.geometry, poly);
+				console.log(isInside);
+				
 				try {
-					// add data to DOM if successful
 					if (geocoderControl == false) {
-						addAndPopulateLinks(layer.properties.ATT, layer.properties.CenturyLink, 
+						addAndPopulateLinks(layer.properties.ATT, layer.properties.CenturyLin, 
 							layer.properties.Charter, layer.properties.Comcast, layer.properties.Frontier, 
-							layer.properties.Mediacom, layer.properties.Midco, layer.properties.Sprint, 
-							layer.properties.Lifeline);
+							layer.properties.Mediacom, layer.properties.Midco, layer.properties.Sprint, layer.properties.Lifeline); //layer.properties.Sprint,
 						geocoderControl = true;
 					}
 				} catch(err) {
-					// display error message to DOM if unsuccessful
 					if (geocoderControl == false) {
 						catchUndefinedLayer(err);
 						geocoderControl = true;
 					}
+					
 				}
+					
+					
 			} else {
-				// if not at 100%, add 1 to width (smaller value creates slower geocoder load)
 				width = width + .5;
 				elem.style.width = width + '%';
+				//elem.innerHTML = width * 1 + '%';
 			}
 		};
 	};
 	
 	// function to add and populate links
-	function addAndPopulateLinks(ATT, CenturyLink, Charter, Comcast, Frontier, Mediacom, Midco, Sprint, Lifeline) {
+	function addAndPopulateLinks(ATT, CenturyLin, Charter, Comcast, Frontier, Mediacom, Midco, Sprint, Lifeline) {
 		errorCount = 0;
 		// create links
 		var link1 = document.createElement("a");
@@ -234,18 +239,18 @@ var geoC = (function() {
 		link8.setAttribute("target", "_blank");
 		link9.setAttribute("target", "_blank");
 		
-		// check if properties has link: yes link = give href || no link = no href, class gonnaRemove
+		// check if properties has link
 		if (ATT != "No discount program") {
 			link1.setAttribute("href", ATT);
-			link1.innerHTML = "<b>ATT: </b>Discount Program Available"; //+ ATT;
+			link1.innerHTML = "<b>ATT: </b>Discount Program Available";// + ATT;
 		} else {
 			link1.innerHTML = "<b>ATT: </b>No discount program";
 			link1.setAttribute("class", "noLink gonnaRemove");
 		}
 		
-		if (CenturyLink != "No discount program") {
-			link2.setAttribute("href", CenturyLink);
-			link2.innerHTML = "<b>CenturyLink: </b>Discount Program Available"; //+ CenturyLink;
+		if (CenturyLin != "No discount program") {
+			link2.setAttribute("href", CenturyLin);
+			link2.innerHTML = "<b>CenturyLink: </b>Discount Program Available";//  + CenturyLink;
 		} else {
 			link2.innerHTML = "<b>CenturyLink: </b>No discount program";
 			link2.setAttribute("class", "noLink gonnaRemove");
@@ -253,7 +258,7 @@ var geoC = (function() {
 		
 		if (Charter != "No discount program") {
 			link3.setAttribute("href", Charter);
-			link3.innerHTML = "<b>Charter: </b>Discount Program Available"; //+ Charter;
+			link3.innerHTML = "<b>Charter: </b>Discount Program Available";//  + Charter;
 		} else {
 			link3.innerHTML = "<b>Charter: </b>No discount program";
 			link3.setAttribute("class", "noLink gonnaRemove");
@@ -261,7 +266,7 @@ var geoC = (function() {
 		
 		if (Comcast != "No discount program") {
 			link4.setAttribute("href", Comcast);
-			link4.innerHTML = "<b>Comcast: </b>Discount Program Available"; //+ Comcast;
+			link4.innerHTML = "<b>Comcast: </b>Discount Program Available";//  + Comcast;
 		} else {
 			link4.innerHTML = "<b>Comcast: </b>No discount program";
 			link4.setAttribute("class", "noLink gonnaRemove");
@@ -269,7 +274,7 @@ var geoC = (function() {
 		
 		if (Frontier != "No discount program") {
 			link5.setAttribute("href", Frontier);
-			link5.innerHTML = "<b>Frontier: </b>Discount Program Available"; //+ Frontier;
+			link5.innerHTML = "<b>Frontier: </b>Discount Program Available";//  + Frontier;
 		} else {
 			link5.innerHTML = "<b>Frontier: </b>No discount program";
 			link5.setAttribute("class", "noLink gonnaRemove");
@@ -277,7 +282,7 @@ var geoC = (function() {
 		
 		if (Mediacom != "No discount program") {
 			link6.setAttribute("href", Mediacom);
-			link6.innerHTML = "<b>Mediacom: </b>Discount Program Available"; //+ Mediacom;
+			link6.innerHTML = "<b>Mediacom: </b>Discount Program Available";//  + Mediacom;
 		} else {
 			link6.innerHTML = "<b>Mediacom: </b>No discount program";
 			link6.setAttribute("class", "noLink gonnaRemove");
@@ -285,7 +290,7 @@ var geoC = (function() {
 		
 		if (Midco != "No discount program") {
 			link7.setAttribute("href", Midco);
-			link7.innerHTML = "<b>Midco: </b>Discount Program Available"; //+ Midco;
+			link7.innerHTML = "<b>Midco: </b>Discount Program Available";//  + Midco;
 		} else {
 			link7.innerHTML = "<b>Midco: </b>No discount program";
 			link7.setAttribute("class", "noLink gonnaRemove");
@@ -293,7 +298,7 @@ var geoC = (function() {
 		
 		if (Sprint != "No discount program") {
 			link8.setAttribute("href", Sprint);
-			link8.innerHTML = "<b>Sprint: </b>Discount Program Available"; //+ Sprint;
+			link8.innerHTML = "<b>PCs for people: </b>Discount Program Available";//  + PCs for people;
 		} else {
 			link8.innerHTML = "<b>Sprint: </b>No discount program";
 			link8.setAttribute("class", "noLink gonnaRemove");
@@ -301,11 +306,12 @@ var geoC = (function() {
 		
 		if (Lifeline != "No discount program") {
 			link9.setAttribute("href", Lifeline);
-			link9.innerHTML = "<b>Lifeline: </b>Discount Program Available"; //+ Lifeline;
+			link9.innerHTML = "<b>Lifeline: </b>Discount Program Available";//  + Lifeline;
 		} else {
 			link9.innerHTML = "<b>Lifeline: </b>No discount program";
 			link9.setAttribute("class", "noLink gonnaRemove");
 		}
+		
 		
 		// append links and breaks to container
 		$(link1).appendTo("#myContainer");
@@ -330,11 +336,10 @@ var geoC = (function() {
 		// set timer to allow function to run again
 		window.setTimeout(function() {
 			geocoderControl = false;
-		}, 1000);
+		}, 7000);
 	};
 	
 	function catchUndefinedLayer(err) {
-		// determines which error message to display on unsuccessful query
 		errorCount = errorCount + 1;
 		var myErrorText = document.createElement("h3");
 		myErrorText.setAttribute("class", "gonnaRemove");
@@ -348,7 +353,7 @@ var geoC = (function() {
 		// set timer to allow function to run again
 		window.setTimeout(function() {
 			geocoderControl = false;
-		}, 1000);
+		}, 7000);
 	};
 	
 })();
