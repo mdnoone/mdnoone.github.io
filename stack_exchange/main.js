8f38723bc010f53c6420805b1b1b65ade87271e0
mdnoone <matthew.d.noone@gmail.com>
2018-07-11 10:39:53 -0500

    stack

--- stack_exchange/main.js
+++ stack_exchange/main.js
@@ -186,57 +186,22 @@ var geoC = (function() {
 		// create links
 		var link1 = document.createElement("a");
 		var link2 = document.createElement("a");
-		/*var link3 = document.createElement("a");
-		var link4 = document.createElement("a");
-		var link5 = document.createElement("a");
-		var link6 = document.createElement("a");
-		var link7 = document.createElement("a");
-		var link8 = document.createElement("a");
-		var link9 = document.createElement("a");*/
 		
 		// create breaks
 		var break1 = document.createElement("br");
 		var break2 = document.createElement("br");
-		/*var break3 = document.createElement("br");
-		var break4 = document.createElement("br");
-		var break5 = document.createElement("br");
-		var break6 = document.createElement("br");
-		var break7 = document.createElement("br");
-		var break8 = document.createElement("br");
-		var break9 = document.createElement("br");*/
 		
 		// attribute links with 'gonnaRemove' class
 		link1.setAttribute("class", "gonnaRemove");
 		link2.setAttribute("class", "gonnaRemove");
-		/*link3.setAttribute("class", "gonnaRemove");
-		link4.setAttribute("class", "gonnaRemove");
-		link5.setAttribute("class", "gonnaRemove");
-		link6.setAttribute("class", "gonnaRemove");
-		link7.setAttribute("class", "gonnaRemove");
-		link8.setAttribute("class", "gonnaRemove");
-		link9.setAttribute("class", "gonnaRemove");*/
 		
 		// attribute breaks with 'gonnaRemove' class
 		break1.setAttribute("class", "gonnaRemove");
 		break2.setAttribute("class", "gonnaRemove");
-		/*break3.setAttribute("class", "gonnaRemove");
-		break4.setAttribute("class", "gonnaRemove");
-		break5.setAttribute("class", "gonnaRemove");
-		break6.setAttribute("class", "gonnaRemove");
-		break7.setAttribute("class", "gonnaRemove");
-		break8.setAttribute("class", "gonnaRemove");
-		break9.setAttribute("class", "gonnaRemove");*/
 		
 		// attribute links with blank target for opening in new tab
 		link1.setAttribute("target", "_blank");
 		link2.setAttribute("target", "_blank");
-		/*link3.setAttribute("target", "_blank");
-		link4.setAttribute("target", "_blank");
-		link5.setAttribute("target", "_blank");
-		link6.setAttribute("target", "_blank");
-		link7.setAttribute("target", "_blank");
-		link8.setAttribute("target", "_blank");
-		link9.setAttribute("target", "_blank");*/
 		
 		// check if properties has link
 		if (Charter != "No discount program") {
@@ -254,83 +219,12 @@ var geoC = (function() {
 			link2.innerHTML = "<b>Norvado: </b>No discount program";
 			link2.setAttribute("class", "noLink gonnaRemove");
 		}
-		
-		/*if (Charter != "No discount program") {
-			link3.setAttribute("href", Charter);
-			link3.innerHTML = "<b>Charter: </b>Discount Program Available";//  + Charter;
-		} else {
-			link3.innerHTML = "<b>Charter: </b>No discount program";
-			link3.setAttribute("class", "noLink gonnaRemove");
-		}
-		
-		if (Comcast != "No discount program") {
-			link4.setAttribute("href", Comcast);
-			link4.innerHTML = "<b>Comcast: </b>Discount Program Available";//  + Comcast;
-		} else {
-			link4.innerHTML = "<b>Comcast: </b>No discount program";
-			link4.setAttribute("class", "noLink gonnaRemove");
-		}
-		
-		if (Frontier != "No discount program") {
-			link5.setAttribute("href", Frontier);
-			link5.innerHTML = "<b>Frontier: </b>Discount Program Available";//  + Frontier;
-		} else {
-			link5.innerHTML = "<b>Frontier: </b>No discount program";
-			link5.setAttribute("class", "noLink gonnaRemove");
-		}
-		
-		if (Mediacom != "No discount program") {
-			link6.setAttribute("href", Mediacom);
-			link6.innerHTML = "<b>Mediacom: </b>Discount Program Available";//  + Mediacom;
-		} else {
-			link6.innerHTML = "<b>Mediacom: </b>No discount program";
-			link6.setAttribute("class", "noLink gonnaRemove");
-		}
-		
-		if (Midco != "No discount program") {
-			link7.setAttribute("href", Midco);
-			link7.innerHTML = "<b>Midco: </b>Discount Program Available";//  + Midco;
-		} else {
-			link7.innerHTML = "<b>Midco: </b>No discount program";
-			link7.setAttribute("class", "noLink gonnaRemove");
-		}
-		
-		if (Sprint != "No discount program") {
-			link8.setAttribute("href", Sprint);
-			link8.innerHTML = "<b>PCs for people: </b>Discount Program Available";//  + PCs for people;
-		} else {
-			link8.innerHTML = "<b>Sprint: </b>No discount program";
-			link8.setAttribute("class", "noLink gonnaRemove");
-		}
-		
-		if (Lifeline != "No discount program") {
-			link9.setAttribute("href", Lifeline);
-			link9.innerHTML = "<b>Lifeline: </b>Discount Program Available";//  + Lifeline;
-		} else {
-			link9.innerHTML = "<b>Lifeline: </b>No discount program";
-			link9.setAttribute("class", "noLink gonnaRemove");
-		}*/
-		
-		
+	
 		// append links and breaks to container
 		$(link1).appendTo("#myContainer");
 		$(break1).appendTo("#myContainer");
 		$(link2).appendTo("#myContainer");
 		$(break2).appendTo("#myContainer");
-		/*$(link3).appendTo("#myContainer");
-		$(break3).appendTo("#myContainer");
-		$(link4).appendTo("#myContainer");
-		$(break4).appendTo("#myContainer");
-		$(link5).appendTo("#myContainer");
-		$(break5).appendTo("#myContainer");
-		$(link6).appendTo("#myContainer");
-		$(break6).appendTo("#myContainer");
-		$(link7).appendTo("#myContainer");
-		$(break7).appendTo("#myContainer");
-		$(link8).appendTo("#myContainer");
-		$(break8).appendTo("#myContainer");
-		$(link9).appendTo("#myContainer");
-		$(break9).appendTo("#myContainer");*/
 		
 		// set timer to allow function to run again
 		window.setTimeout(function() {
